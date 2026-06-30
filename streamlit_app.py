0e6354f9b195fffbf83577b05531893a59457e45
Ashish Birle <ashish.birle13@gmail.com>
2026-06-30 15:18:50 +0530

    background imaged added to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,27 @@ import pandas as pd
 from e_commerce_recommendation import product_recommendations
 import streamlit as st
 
+image_url = "https://raw.githubusercontent.com/ashishbirle/product-recommendation-engine/refs/heads/main/background_image/img_recommendation.png"
+
+
+def set_github_background(url):
+    page_bg_img = f'''
+    <style>
+    [data-testid="stAppViewContainer"] {{
+        background-image: url("{url}");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: local;
+    }}
+    </style>
+    '''
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_github_background(image_url)
+# The above code is to set up a background image for to the streamlit application.
+
 st.title("Welcome to Product Recommendation Engine")
 df = pd.read_csv("dataset/indian_ecommerce_dataset.csv")
 
