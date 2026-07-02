9d219e7eacf69d80a8db1920287ad837d0d0078d
Ashish Birle <ashish.birle13@gmail.com>
2026-07-02 15:09:37 +0530

    Added Project Report along with a modification (Price Filter)

--- e_commerce_recommendation.py
+++ e_commerce_recommendation.py
@@ -21,7 +21,7 @@ tfidf_matrix = tfidf.fit_transform(df['Features'])
 cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
 # Function to get product recommendations based on a given product index
-def recommend_products(product_name):
+def recommend_products(product_name, min_price=0, max_price=150000):
     # Get the index of the product that matches the name
     idx = df[df['Product_Name'] == product_name].index[0]
     
@@ -32,11 +32,13 @@ def recommend_products(product_name):
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     
     # Get the top 5 most similar products
-    sim_scores = sim_scores[1:6]
+    # sim_scores = sim_scores[1:6]
     
-    # Get the product indices
-    product_indices = [i[0] for i in sim_scores]
-    
+    # sim_scores = sim_scores[1:6]
+    # if max_price:
+    product_indices = [i for i,s in sim_scores if min_price <= df['Price'][i] <= max_price][1:6]
+    # else:    
+    #     product_indices = [x[0] for x in sim_scores][1:6]
     # Return the recommended products
     return df[['Product_Name','Category','Brand','Price','Rating']].iloc[product_indices]
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -26,21 +26,25 @@ set_github_background(image_url)
 st.title("Welcome to Product Recommendation Engine")
 df = pd.read_csv("dataset/indian_ecommerce_dataset.csv")
 
-validity = 0
+
 try:
     product_name = st.text_input("Enter the product name:")
-    if st.button("Check validity"):
-        if not product_name:
-            raise ValueError("Product name cannot be empty.")
-        elif product_name not in df['Product_Name'].values:
-            raise ValueError("Product not found in the dataset.")
-        else:
-            st.write("Valid Product")
+    col1, col2 = st.columns([1,3], vertical_alignment="center")
+    with col1:
+        if st.button("Check validity"):
+            if not product_name:
+                raise ValueError("Product name cannot be empty.")
+            elif product_name not in df['Product_Name'].values:
+                raise ValueError("Product not found in the dataset.")
+            else:
+                st.write("Valid Product")
+    with col2:
+        values = st.slider('Price Range',min_value=0, max_value=150000, value = (0,150000), step=5000)
 
 except ValueError as e:
-    st.write(f"Error: {e}")
+        st.write(f"Error: {e}")
 
 
 if st.button("Recommend"):
-    recommendation = product_recommendations(product_name)
+    recommendation = product_recommendations(product_name, values[0], values[1])
     st.write(recommendation)
